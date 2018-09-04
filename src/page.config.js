a28dbd4ee6c79c099520da6ea60001d5f6fc1681
xilanhuadechuntian <Lensgcx@163.com>
2018-09-04 21:27:51 +0800

    page.config
    page.config

---
+++ src/page.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+module.exports = {
+    command: {
+        css: [],
+        js: [
+            {name: 'main', path: './src/main.js'},
+        ],
+        'x-icon': 'http://otaflb4oo.bkt.clouddn.com/CX-UI-Design/Logo/bigBear.ico'
+    },
+    pages: [
+        {
+            name: 'index',
+            template: 'src/pages/home/index.ejs',
+            output: 'dist/index.html',
+            jsEntry: 'src/pages/home/index.js',
+            content: {
+                title: 'My interviewMap | Home',
+                description: 'Home Page'
+            },
+        },
+        {
+            name: 'about',
+            template: 'src/pages/about/about.ejs',
+            output: 'dist/pages/about.html',
+            jsEntry: 'src/pages/about/about.js',
+            content: {
+                title: 'My interviewMap | about',
+                description: 'about Page'
+            },
+        },
+        {
+            name: 'about',
+            template: 'src/pages/contact/contact.ejs',
+            output: 'dist/pages/contact.html',
+            jsEntry: 'src/pages/contact/contact.js',
+            content: {
+                title: 'My interviewMap | contact',
+                description: 'contact Page'
+            },
+        }
+    ]
+};
