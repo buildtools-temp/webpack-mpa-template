11d12f9e08f5517c391a70edbb5fec58dd4f6cd6
xilanhuadechuntian <Lensgcx@163.com>
2018-09-04 21:27:55 +0800

    main
    main

---
+++ src/main.js
@@ -0,0 +1,22 @@
+//amin CSS
+import pageConfig from './page.config'
+
+
+import {dynamicLoading} from './utils/index'
+
+
+{
+    init();
+}
+
+function init() {
+    console.log('初始化开始');
+    // const command = pageConfig.command;
+    // dynamicLoading.css(command['x-icon'], "icon", "image/x-icon");
+    // dynamicLoading.css(command['x-icon'], "shortcut icon", "image/x-icon");
+    // // dynamicLoading.css('https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css');
+    // // dynamicLoadig.js('https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js');
+    // // dynamicLoading.js('https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js');
+    // console.log('加载完毕');
+}
+
