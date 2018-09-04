9cba5cf5a3642bc89ccdc4ac84fc99c4eeed5751
xilanhuadechuntian <Lensgcx@163.com>
2018-09-04 21:29:29 +0800

    home script

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+
+
+$(document).ready(readyHandler);
+
+function readyHandler() {
+    console.log('首页的脚本js运行了～～');
+    $('#interviewMap').css('color', 'red');
+
+}
+
+
+
+
+
