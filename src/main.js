a915c7ec2c2db8a9ea1ed706826dbd8dd94633e4
xilanhuadechuntian <Lensgcx@163.com>
2018-09-14 14:45:01 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,25 +1,9 @@
-//amin CSS
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/css/main.scss'
-import pageConfig from './page.config';
+import 'jquery'
+import 'bootstrap';
+import './assets/css/main.scss';
 
+$(document).ready(() => {
+    console.log('main.js loaded');
+});
 
-import {dynamicLoading} from './utils/index';
-
-
-{
-    init();
-}
-
-function init() {
-    console.log('初始化开始');
-    // const command = pageConfig.command;
-    // dynamicLoading.css(command['x-icon'], "icon", "image/x-icon");
-    // dynamicLoading.css(command['x-icon'], "shortcut icon", "image/x-icon");
-    // // dynamicLoading.css('https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css');
-    // // dynamicLoadig.js('https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js');
-    // // dynamicLoading.js('https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js');
-    // console.log('加载完毕');
-}
 
