f3ad4a78a4170c285013e193a1f9e864a48cb52c
xilanhuadechuntian <Lensgcx@163.com>
2018-09-14 14:40:16 +0800

    webpack.dev.conf
    webpack.dev.conf

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,30 +1,28 @@
-'use strict';
 const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const utils = require('./utils');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const portfinder = require('portfinder');
 
-const utils = require('./utils');
-
-//webpack config
 const baseWebpackConfig = require('./webpack.base.conf');
 const config = require('../config');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT && Number(process.env.PORT);
 
-/*-------------------------------------------------------*/
-/*   merge config (base and dev)             			 */
-/*   将两个配置对象，进行合并( 合并了base中的webpack配置项 )  */
-/*-------------------------------------------------------*/
-const devWebpackConfig = merge(baseWebpackConfig, {
+const webpackConfig = {
+    mode: 'development', // 通过 mode 声明开发环境
+    output: {
+        path: config.dev.assetsRoot,
+        // 打包多出口文件
+        filename: utils.assetsPath('js/[name].bundle.js'),
+        publicPath:config.dev.assetsPublicPath
+    },
+    // cheap-module-eval-source-map is faster for development
+    // 增加了 devtools，通过注释的英文翻译，可以知道cheap-module-eval-source-map使得开发更快
+    devtool: config.dev.devtool,
 
-    //devSever的一些配置项了。其中包块客户端报错级别、端口、host等等
     devServer: {
         /**
          * 当使用内联模式(inline mode)时，会在开发工具(DevTools)的控制台(console)显示消息, 可能的值有 none, error, warning 或者 info（默认值）
@@ -32,7 +30,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         clientLogLevel: 'warning',
         contentBase: config.dev.contentBase,// since we use CopyWebpackPlugin. false, 告诉服务器从哪个目录中提供内容。只有在你想要提供静态文件时才需要
         /**
-         * 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.ejs。通过传入以下启用：
+         * 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 about.html。通过传入以下启用：
          * 通过传入一个对象，比如使用 rewrites 这个选项，此行为可进一步地控制：
          */
         historyApiFallback: {
@@ -41,116 +39,58 @@ const devWebpackConfig = merge(baseWebpackConfig, {
             ],
         },
         publicPath: config.dev.assetsPublicPath,
-        host: HOST || config.dev.host, //默认是 localhost
+        host: HOST || config.dev.host,//默认是 localhost
         port: PORT || config.dev.port,//指定要监听请求的端口号
         inline: true,//注意：不写hot: true，否则浏览器无法自动更新；也不要写colors:true，progress:true等，webpack2.x已不支持这些
         open: config.dev.autoOpenBrowser,//是否自动打开浏览器
         hot: config.dev.hot || true,//启用 webpack 的模块热替换特性
         overlay: config.dev.errorOverlay,//当出现编译器错误或警告时，在浏览器中显示全屏覆盖层。默认禁用。
-        quiet: config.dev.quiet,//启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台。这也意味着来自 webpack 的错误或警告在控制台不可见。
-
+        /**
+         * 启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台。这也意味着来自 webpack 的错误或警告在控制台不可见。
+         * 可在之后的 portfinder 中定义输出的内容
+         **/
+        quiet: config.dev.quiet,
+        stats: "errors-only", //stats: "errors-only"表示只打印错误：
+        //服务器代理配置项
+        proxy: {
+            '/test/*': {
+                target: 'https://www.baidu.com',
+                secure: true,
+                changeOrigin: true
+            }
+        }
     },
-    //新增的plugins
     plugins: [
-        require('autoprefixer'),
         //定义process.env
         new webpack.DefinePlugin({
             'process.env': require('../config/dev.env')
         }),
         // 模块热替换插件
         new webpack.HotModuleReplacementPlugin(),
+        //显示模块加载相对路径插件
+        new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+        // 在编译出现错误时，使用 NoEmitOnErrorsPlugin 来跳过输出阶段。这样可以确保输出资源不会包含错误
+        new webpack.NoEmitOnErrorsPlugin(),
 
-        new ExtractTextPlugin({
-            filename: 'static/css/[name].[hash:7].css'
-        }),
-        //设置每一次build之前先删除dist
-        new CleanWebpackPlugin(
-            ['dist/*',],　     //匹配删除的文件
-            {
-                root: __dirname,   //根目录
-                verbose: true,    //开启在控制台输出信息
-                dry: false     //启用删除文件
-            }
-        ),
-        // copy custom static assets
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, '../static'),
-                to: config.dev.assetsSubDirectory,
-                ignore: ['.*']
-            }
-        ]),
-    ]
-});
+        // new webpack.DefinePlugin({
+        //     'process.env.BASE_URL': '\"' + process.env.BASE_URL + '\"'
+        // })
+    ],
 
+    module: {
+        rules: []
+    },
+};
 
-//page config information
-const commandConf = require('../src/page.config').command;
-const pageConf = require('../src/page.config').pages;
+const devWebpackConfig = merge(baseWebpackConfig, webpackConfig);
 
-/*-------------------------------*/
-/*       add command entry       */
-/*-------------------------------*/
-if (commandConf) {
-    const CommandJSConf = commandConf.js;
-    if (CommandJSConf && Array.isArray(CommandJSConf)) {
-        CommandJSConf.map(conf => {
-            devWebpackConfig.entry[conf.name] = conf.path;
-        })
-    }
-}
-
-
-/*-------------------------------*/
-/*     add entry and plugins     */
-/*-------------------------------*/
-if (pageConf && Array.isArray(pageConf)) {
-    pageConf.map(page => {
-
-        if (page.jsEntry && page.name) {
-            let _Entry = [];
-            //entry list push new js-entry script
-            _Entry.push(
-                path.resolve(page.template),//page for current page
-                path.resolve(page.jsEntry),//script for current page
-            );
-            devWebpackConfig.entry[page.name] = _Entry;
-        }
-
-        const defaultTit = 'My interviewMap';
-        //add new htmlWebpackPlugin to plugins list
-        devWebpackConfig.plugins.push(new HtmlWebpackPlugin({
-            /*
-             * 1、 path.join(__dirname, url),  __dirname变量值代表 程序 运行的根目录。
-             * 2、 path.resolve 方法该方法以应用程序 根目录 为起点
-            */
-            template: path.resolve(page.template),
-            filename: path.resolve(page.output),
-            title: page.content.title || defaultTit,
-            inject: true,//js 插入位置（ true, body, head, false ）
-            inlineSource: '.(js|css)$',
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-                // more options:
-                // https://github.com/kangax/html-minifier#options-quick-reference
-            },
-            chunks: ['main', page.name], // 排序顺序
-            chunksSortMode: 'dependency',//'dependency' - 按照不同文件的依赖关系来排序, 'manual' - 手动排序
-        }))
-    })
-}
-
-console.log(11111111111)
-console.log(config.dev.contentBase)
-console.log(11111111111)
-
-
-/*-------------------------------------------------------------------------------------*/
-/*   start by Portfinder           						 															 					 */
-/*   确保启动程序时，如果端口被占用时，会通过portfinder来发布新的端口，然后输出运行的host字符串  */
-/*-------------------------------------------------------------------------------------*/
+/**
+ * start by Portfinder
+ * 确保启动程序时，如果端口被占用时，会通过portfinder来发布新的端口，然后输出运行的host字符串
+ * https://npm.taobao.org/package/portfinder
+ * https://npm.taobao.org/package/friendly-errors-webpack-plugin
+ * @type {Promise<any>}
+ */
 module.exports = new Promise((resolve, reject) => {
     portfinder.basePort = process.env.PORT || config.dev.port;
     portfinder.getPort((err, port) => {
@@ -163,9 +103,13 @@ module.exports = new Promise((resolve, reject) => {
             devWebpackConfig.devServer.port = port;
 
             // Add FriendlyErrorsPlugin
+            // Add FriendlyErrorsPlugin
             devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
                 compilationSuccessInfo: {
-                    messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+                    notes: [`Create by Broccoli spring( gcx ) <Lensgcx@163.com>: https://github.com/Lensgcx`],
+                    messages: [
+                        `Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`,
+                    ],
                 },
                 onErrors: config.dev.notifyOnErrors
                     ? utils.createNotifierCallback()
